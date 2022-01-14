57bd551734b947670f856c55d721c6b9c1e78a44
Martin Winter <dev@macwinnie.me>
2022-01-14 15:05:57 +0100

    fix some errors occuring while bootup
    * non-installed NC is returned as STDERR instead of STDOUT
    * function from our entrypoint is not available in NC entrypoint

--- files/boot.d/nextcloud/01_occ.sh
+++ files/boot.d/nextcloud/01_occ.sh
@@ -4,10 +4,11 @@ check_file="/entrypoint_ran_scripts"
 
 if [ ! -f "${check_file}" ]; then
 
-    check=$( sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ -V )
+    check=$( sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ -V 2>&1 )
     check=$( echo ${check} | sed 's/./\L&/g' )
 
     if [[ ! ${check} =~ "not installed" ]]; then
+
         sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ upgrade
         sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ db:add-missing-indices
         sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ maintenance:update:htaccess
