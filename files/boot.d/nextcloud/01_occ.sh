79b77b5a0625eb2ed47e4672cc29f26de15bc2d5
Martin Winter <dev@macwinnie.me>
2022-01-14 14:54:50 +0100

    run `occ` commands only if container is newly created at bootup

--- files/boot.d/nextcloud/01_occ.sh
+++ files/boot.d/nextcloud/01_occ.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-check=$( sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ -V )
-check=$( echo ${check} | sed 's/./\L&/g' )
+check_file="/entrypoint_ran_scripts"
 
-if [[ ! ${check} =~ "not installed" ]]; then
-    sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ upgrade
-    sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ db:add-missing-indices
-    sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ maintenance:update:htaccess
+if [ ! -f "${check_file}" ]; then
 
-    echo "${NC_APPS}" | sudo -u ${NC_USER} xargs -d ' ' -n1 php -d memory_limit=-1 ${NC_BASE_PATH}/occ app:install
-    echo "${NC_APPS}" | sudo -u ${NC_USER} xargs -d ' ' -n1 php -d memory_limit=-1 ${NC_BASE_PATH}/occ app:enable
+    check=$( sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ -V )
+    check=$( echo ${check} | sed 's/./\L&/g' )
+
+    if [[ ! ${check} =~ "not installed" ]]; then
+        sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ upgrade
+        sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ db:add-missing-indices
+        sudo -u ${NC_USER} php -d memory_limit=-1 ${NC_BASE_PATH}/occ maintenance:update:htaccess
+
+        echo "${NC_APPS}" | sudo -u ${NC_USER} xargs -d ' ' -n1 php -d memory_limit=-1 ${NC_BASE_PATH}/occ app:install
+        echo "${NC_APPS}" | sudo -u ${NC_USER} xargs -d ' ' -n1 php -d memory_limit=-1 ${NC_BASE_PATH}/occ app:enable
+
+        touch "${check_file}"
+    fi
 fi
